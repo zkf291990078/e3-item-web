555d2bbab0aa5420455fe5e625d8de680c50102d
zhangkunfeng <291990078@qq.com>
2018-01-25 20:53:37 +0800

    修改详情controller

--- src/main/java/cn/e3mall/item/controller/ProductItemController.java
+++ src/main/java/cn/e3mall/item/controller/ProductItemController.java
@@ -1,6 +1,7 @@
 package cn.e3mall.item.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +11,13 @@ import cn.e3mall.item.pojo.Item;
 import cn.e3mall.pojo.TbItem;
 import cn.e3mall.pojo.TbItemDesc;
 import cn.e3mall.service.ItemService;
-
+@Controller
 public class ProductItemController {
 
 	@Autowired
 	private ItemService itemService;
 
 	@RequestMapping("/item/{itemId}")
-	@ResponseBody
 	public String showItemInfo(@PathVariable long itemId, Model model) {
 		TbItem tbItem = itemService.getItemById(itemId);
 		Item item = new Item(tbItem);
